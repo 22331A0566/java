797b7bef010234282861cf0127730734fbf948d2
22331A0566 <158837338+22331A0566@users.noreply.github.com>
2024-02-04 22:06:19 +0530

    Create linux.c

---
+++ week1/linux.c
@@ -0,0 +1,7 @@
+class Thready extends Thread{
+	public static void main(String[] args){
+   	 Thready Thread1 = new Thready();
+    	 Thread1.setName("Rocky");
+   	 System.out.println(Thread1.getName());
+    }
+}
