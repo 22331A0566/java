e1eeb1fd63f58adca2aa08b93bb996c8c9363f63
22331A0566 <158837338+22331A0566@users.noreply.github.com>
2024-04-30 23:22:53 +0530

    Create mapinterJava.java

---
+++ week10/mapinterJava.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+public class mapinterJava {
+public static void main(String[] args) {
+Map<String, String> SId = new HashMap<>();
+SId.put("gayii", "501");
+SId.put("siddikha", "523");
+SId.put("harshi", "566");
+SId.put("keerthana", "550");
+for (Map.Entry<String, String> entry : SId.entrySet()) {
+String name = entry.getKey();
+String Id = entry.getValue();
+System.out.println(name + " ID: " + Id);
+}
+System.out.println("\n");
+System.out.println("Changing -> gayii : 501 - l501");
+SId.put("gayii", "l501");
+System.out.println("Removing -> keerthana : 550");
+SId.remove("keerthana");
+boolean sidd = SId.containsKey("siddikha");
+System.out.println("Is there any Student Id with siddikha ? --> " + sidd);
+System.out.println("\n");
+for (Map.Entry<String, String> entry : SId.entrySet()) {
+String name = entry.getKey();
+String Id = entry.getValue();
+System.out.println(name + " ID: " + Id);
+}
+}
+}
+
